33f4c9e783f4d5a2b9d714534c543442e09a80fb
repl.it user <replituser@example.com>
2020-12-02 15:57:15 +0000

    Problem 2 d+e

--- likelihood.cc
+++ likelihood.cc
@@ -45,11 +45,37 @@ int main() {
   }
   lllhfile.close();
 
-    ofstream nllfile("deltanll.txt");
+
+  ofstream nllfile("deltanll.txt");
   for(double mu = 0 ; mu <= 6 ; mu += 0.01){
-    nllfile << mu << " " << -2*log(prob(daten, mu)) + 2* log(prob(daten, mu_true) )<< endl;
+    nllfile << mu << " " << -2*log(prob(daten, mu)) + 2* log(prob(daten, mu_true))<< endl;
   }
   nllfile.close();
 
+double mu_est, mu_maxstd, mu_minstd, mu_std, std_sample, denominator, lambda;
+// estimate for mu_true from plot : 3.12
+  mu_est = 3.12;
+// estimate function ==1 in plot
+  mu_minstd = 3.0;
+  mu_maxstd = 3.23;
+  mu_std = (mu_maxstd - mu_minstd)/2;
+
+//compare to uncertainty in sample mean
+std_sample = 1.65365 / sqrt(234);
+cout << "Std from Llh: " << mu_std << endl;
+cout << "Std of sample mean: " << std_sample <<endl;
+// are the same wit h respect to our accuracy of 0.01
+
+denominator = 1;
+for(unsigned int k = 0 ; k < daten.size() ; ++k){
+  denominator *= poisson(daten[k], daten[k]);
 }
-    
+
+lambda = prob(daten,mu_true) / denominator;
+cout << "likelihood ratio: \n";
+cout << lambda << endl;
+
+double z;
+z = (-2* log(lambda) - 233) / (sqrt(466));
+cout << z << endl;
+}    
